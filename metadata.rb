3c35cf14f597f9af66c9798da7fcbfb8d9087e58
Christian Rodriguez <car@cespi.unlp.edu.ar>
2013-07-02 13:24:43 -0300

    sacamos dependencias de apt-proxy

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,8 @@ description      "Installs/Configures postfix with ldap support"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "apt"
 depends 'certificate'
 depends 'clamav'
-depends "apt"
 depends "certificate"
 depends "clamav"
-depends "apt-proxy"
